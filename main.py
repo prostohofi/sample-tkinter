1f27ed487d8bf0c0d137891a6271ba3edae8ac46
prostohofi <145482767+prostohofi@users.noreply.github.com>
2024-04-12 19:41:28 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import tkinter
+
+
+class App:
+    """приложение"""
+    def __init__(self, width: int, height: int) -> None:
+        self.window = tkinter.Tk()
+        self.width = width
+        self.height = height
+        self.window.geometry(f"{self.width}x{self.height}")
+        self.window.mainloop()
+
+
+App(1024, 1600)
